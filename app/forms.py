08966ad1f1cfdc1739e87888c96f8d0e79e9af2a
Billy Ceskavich <bceskavich@gmail.com>
2014-04-15 00:38:02 -0400

    Style changes to the calculator page.

--- app/forms.py
+++ app/forms.py
@@ -3,9 +3,9 @@ from wtforms import IntegerField, SelectField
 from wtforms.validators import Required
 
 class BACForm(Form):
-    beer = IntegerField('beer', default = 0)
-    wine = IntegerField('wine', default = 0)
-    liqour = IntegerField('liqour', default = 0)
-    hours = IntegerField('hours', default = 0, validators = [Required()])
-    weight = IntegerField('weight', default = 0)
-    gender = SelectField('gender', choices=[('male', 'M'), ('female', 'F')])
+    beer = IntegerField('beer', default = 0, id="bac-input")
+    wine = IntegerField('wine', default = 0, id="bac-input")
+    liqour = IntegerField('liqour', default = 0, id="bac-input")
+    hours = IntegerField('hours', default = 0, validators = [Required()], id="bac-input")
+    weight = IntegerField('weight', default = 0, validators = [Required()], id="bac-input")
+    gender = SelectField('gender', choices=[('male', 'Male'), ('female', 'Female')], id="gender")
